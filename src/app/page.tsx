cd9e872e68964d66d01fd9ea0936820e8bdea93f
D. Potikanond <potikanond@gmail.com>
2024-09-30 13:15:41 +0700

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { $authenStore } from "@lib/authenStore";
+import { Course } from "@lib/types";
+import {
+  Button,
+  Group,
+  Loader,
+  Paper,
+  Stack,
+  Text,
+  TextInput,
+  Title,
+} from "@mantine/core";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  // All courses state
+  const [courses, setCourses] = useState<Course[] | null>(null);
+  const [loadingCourses, setLoadingCourses] = useState(false);
+
+  // login state
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loadingLogin, setLoadingLogin] = useState(false);
+
+  // declare useRouter
+
+  const loadCourses = async () => {
+    setLoadingCourses(true);
+    const resp = await axios.get("/api/courses");
+    setCourses(resp.data.courses);
+    setLoadingCourses(false);
+  };
+
+  useEffect(() => {
+    loadCourses();
+  }, []);
+
+  const login = async () => {
+    setLoadingLogin(true);
+    try {
+      const resp = await axios.post("/api/user/login", { username, password });
+      localStorage.setItem("token", resp.data.token);
+      localStorage.setItem("authenUsername", resp.data.username);
+
+      //save token and authenUsername to global store
+
+      //navigate to /student
+    } catch (error) {
+      if (error.response) alert(error.response.data.message);
+      else alert(error.message);
+    }
+    setLoadingLogin(false);
+  };
+
+  return (
+    <Stack>
+      {/* all courses section */}
+      <Paper withBorder p="md">
+        <Title order={4}>All courses</Title>
+
+        {loadingCourses && !courses && <Loader type="dots" />}
+
+        {courses &&
+          courses.map((course) => (
+            <Text key={course.courseNo}>
+              {course.courseNo} - {course.title}
+            </Text>
+          ))}
+      </Paper>
+
+      {/* log in section */}
+      <Paper withBorder p="md">
+        <Title order={4}>Login</Title>
+
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            login();
+          }}
+        >
+          <Group align="flex-end">
+            <TextInput
+              label="Username"
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
+            />
+            <TextInput
+              label="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              type="password"
+            />
+            <Button type="submit" disabled={loadingLogin}>
+              {loadingLogin ? "Login..." : "Login"}
+            </Button>
+          </Group>
+        </form>
+      </Paper>
+    </Stack>
+  );
+}
